b75d3ad8ed0e59bcc888db9568e9f34320ceb51f
alicepetzinger <alice.petzinger@gmail.com>
2021-10-29 10:35:10 +0200

    ➕ set-up files

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import { connectDatabase } from './utils/database';
+import { getBookCollection } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URL dotenv variable');
+}
 
 const app = express();
 const port = 3000;
+
+// For parsing application/json
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
